51f539d7fddf8139a044cd4360f0181f0554f346
sbedellio <sarahbedell42@gmail.com>
2026-04-28 22:39:25 -0400

    Update randomgen-directorswriters.py

--- randomgen-directorswriters.py
+++ randomgen-directorswriters.py
@@ -6,8 +6,9 @@ def choosingrand():
     number = []
     taketh = []
     finalized = []
-    for i in range(9):
+    for i in range(10):
         with open(f"newmovies{i}.csv", "r", encoding='utf-8', newline = '') as csvfile:
+            taketh = []
             file = csv.reader(csvfile)
             for row in file:
                 binary = row[slice(5,6)]
@@ -15,7 +16,7 @@ def choosingrand():
                 number.append([binary, direct])
         print(len(number))
         for j in range(10):
-            taketh.append(random.randint(0, len(number)))
+            taketh.append(random.randint(1, len(number)-1))
             finalized.append([taketh[j],number[taketh[j]]])
             print(f"From newmovies{i}, we have {taketh[j]}: {number[taketh[j]]}")
         number = []
